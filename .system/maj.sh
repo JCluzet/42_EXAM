b66f037e889a8303ef7a3c6191c68b8641a77efa
Joseph CLUZET <jcluzet@e1r12p7.42.fr>
2022-02-14 17:58:03 +0100

    push

--- .system/maj.sh
+++ .system/maj.sh
@@ -1,6 +1,6 @@
 # check if there is connexion to the internet, else do git pull for maj
 if ! ping -c 1 google.com > /dev/null 2>&1; then
-    echo "42_EXAM: Demarrage hors connexion...\n\n"
+    printf "42_EXAM: Demarrage hors connexion...\n\n"
     sleep 2
 else
     git pull
