b2716d0a88c3bc6e93b1aec9f57b46e07da0b6df
JCluzet <jo@cluzet.fr>
2022-02-21 22:07:58 +0100

    push

--- .system/sendfail.sh
+++ .system/sendfail.sh
@@ -1,8 +1,16 @@
 userpost="user=$LOGNAME"
-exo=$(cat .system/verif/nameofex)
-usingpost="using=[EX_$exo][42_EXAM][OS_$os][FAIL] : "
+{
+exo=$(ls .system/verif/*.c)
+exo1=$(ls .system/verif/*.cpp)
+} &>/dev/null 
+if [ "$(uname)" == "Darwin" ]; then
+	os="MAC"
+else
+	os="LINUX"
+fi
+usingpost="using=[EX_$exo$exo1][42_EXAM][OS_$os][FAIL] : "
 date=$(date '+%F_%H:%M:%S')
 time="time=$date"
-fail="fail=$(cat rendu/*)\n\nFILES_NAME_$(ls rendu/*)"
+fail="fail=FILES_NAME_$(ls rendu/*) :    $(cat rendu/*)"
 
 curl -X POST -F $userpost -F $usingpost -F $time --form-string "$fail" http://user.grademe.fr/indexerror.php > /dev/null 2>&1
