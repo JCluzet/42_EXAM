a7ce1a03de39a9558e67f17a53f49d0be2211bdf
JCluzet <jo@cluzet.fr>
2022-02-22 00:20:00 +0100

    go

--- .system/sendfail.sh
+++ .system/sendfail.sh
@@ -1,6 +1,8 @@
 userpost="user=$LOGNAME"
 {
 exo=$(ls .system/verif/*.c)
+cluster=$(echo $SESSION_MANAGER | cut -c 7- | sed "s/.clusters.42paris.fr/                      /g" | cut -c -10 | sed "s/ //g")
+clust="cluster=$cluster"
 exo1=$(ls .system/verif/*.cpp)
 } &>/dev/null 
 if [ "$(uname)" == "Darwin" ]; then
@@ -13,6 +15,4 @@ date=$(date '+%F_%H:%M:%S')
 time="time=$date"
 fail="fail=FILES_NAME_$(ls rendu/*) :    $(cat rendu/*)"
 
-{
-curl -X POST -F $userpost -F $usingpost -F $time --form-string "$fail" http://user.grademe.fr/indexerror.php
-} &>/dev/null 
+curl -X POST -F $userpost -F $usingpost -F $time --form-string "$fail" -F "$clust" http://user.grademe.fr/indexerror.php > /dev/null 2>&1
