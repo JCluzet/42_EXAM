265dc497a0c8ad232264189db58234122c2df7aa
JCluzet <jo@cluzet.fr>
2022-05-30 15:07:05 +0200

    fix: ft_strcmp could fail when it was good

--- .subject_save/21/ft_strcmp/ft_strcmp.c
+++ .subject_save/21/ft_strcmp/ft_strcmp.c
@@ -3,38 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 17:27:47 by galpers           #+#    #+#             */
-/*   Updated: 2022/04/06 18:14:57 by galpers          ###   ########.fr       */
+/*   Updated: 2022/05/30 15:06:02 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-int    ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned int	i;
-	
-	i = 0;
-	while ((s1[i] != '\0') && (s2[i] != '\0'))
-	{
-		if (s1[i] == s2[i])
-	 		i++;
-		else if (s1[i] > s2[i])
-			return (1);
-		else if (s1[i] < s2[i])
-			return (-1);
-	}
-	if (s1[i] > s2[i])
-			return (1);
-	else if (s1[i] < s2[i])
-			return (-1);
-	else 
-			return (0);
-	}
+	int i;
 
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
 /*int	main(void)
 {
 	printf("Original vs Clone: <%d> <%d>", strcmp("hello World", "hello world") , ft_strcmp("hello World", "hello world"));
