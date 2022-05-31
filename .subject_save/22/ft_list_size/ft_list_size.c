51787830da5c99a6a2e3316eb36e5fa87657e1d6
JCluzet <jo@cluzet.fr>
2022-05-31 20:57:00 +0200

    adding correction of ft_list_size

--- .subject_save/22/ft_list_size/ft_list_size.c
+++ .subject_save/22/ft_list_size/ft_list_size.c
@@ -13,26 +13,3 @@ int	ft_list_size(t_list *begin_list)
 	else
 		return (1 + ft_list_size(begin_list->next));
 }
-
-//---------------------------------------------------------------
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int	main(void)
-// {
-// 	int n = 0;
-
-// 	t_list *c = malloc(sizeof(t_list));
-// 	c->next = 0;
-// 	c->data = &n;
-
-// 	t_list *b = malloc(sizeof(t_list));
-// 	b->next = c;
-// 	b->data = &n;
-
-// 	t_list *a = malloc(sizeof(t_list));
-// 	a->next = b;
-// 	a->data = &n;
-
-// 	printf("%d\n", ft_list_size(a));
-// }
