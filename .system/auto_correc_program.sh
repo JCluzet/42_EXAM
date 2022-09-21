38b1379cd27c89ba2ba06b77722db4b40a3f9dac
JCluzet <jo@cluzet.fr>
2022-09-21 13:31:40 +0200

    new: now 42_EXAM detect if your program contain a while loop

--- .system/auto_correc_program.sh
+++ .system/auto_correc_program.sh
@@ -6,13 +6,14 @@
 #    By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/20 02:26:11 by jcluzet           #+#    #+#              #
-#    Updated: 2022/09/01 23:31:21 by jcluzet          ###   ########.fr        #
+#    Updated: 2022/09/21 13:28:19 by jcluzet          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FILE="../../rendu/$2/$1"
 
 tmp=""
+timeout=1
 
 if [ -e .system/grading/traceback ];then
     rm .system/grading/traceback;
@@ -26,8 +27,28 @@ rm source
 gcc -o final "$FILE"
 }  2>.dev
 {
-./final "${@:3}" | cat -e > finalexam        #TESTING
+./final "${@:3}" | cat -e > finalexam &       #TESTING
+PID=$!
 }  &>/dev/null
+
+
+# loop 1 second for 20 sec
+for i in {1..20}
+do
+    sleep 1
+    # if PID is not running, then exit
+    # if i is 5, 10, 15, 19 then echo "waiting..."
+    if [ $i -eq 5 ] || [ $i -eq 10 ] || [ $i -eq 15 ] || [ $i -eq 19 ]; then
+        echo "waiting..."
+    fi
+    if ! ps -p $PID > /dev/null
+    then
+        timeout=0
+        break
+    fi
+done
+
+
 DIFF=$(diff sourcexam finalexam)
 if [ "$DIFF" != "" ]
 then
@@ -35,7 +56,10 @@ then
         printf "        💻 TEST\n./a.out ${@:3}\n" >> traceback
         printf "        🔎 YOUR OUTPUT:\n" >> traceback
         cat finalexam >> traceback
-		if [ -e final ]
+        if [ $timeout -eq 1 ]
+        then
+        printf "   ❌ TIMEOUT\n" >> traceback
+		elif [ -e final ]
 		then
         printf "        🗝 EXPECTED OUTPUT:\n" >> traceback
 		cat sourcexam >> traceback
