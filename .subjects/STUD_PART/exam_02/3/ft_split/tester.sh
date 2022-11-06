affac6be1eecd09eb438933710ee20457b44a269
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-11-06 22:31:03 +0100

    added test for ft_split

--- .subjects/STUD_PART/exam_02/3/ft_split/tester.sh
+++ .subjects/STUD_PART/exam_02/3/ft_split/tester.sh
@@ -6,7 +6,7 @@
 #    By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/06 16:00:29 by pandalaf          #+#    #+#              #
-#    Updated: 2022/11/06 16:05:38 by pandalaf         ###   ########.fr        #
+#    Updated: 2022/11/06 22:29:59 by pandalaf         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,12 @@ FILE='ft_split.c'
 ASSIGN='ft_split'
 
 bash .system/auto_correc_main.sh $FILE $ASSIGN "three words apart"
+if [ -e .system/grading/traceback ];then
+    mv .system/grading/traceback .
+	exit 1
+fi
+
+bash .system/auto_correc_main.sh $FILE $ASSIGN "  starting and ending   "
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
