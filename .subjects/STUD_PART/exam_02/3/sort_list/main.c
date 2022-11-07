b91e10ed52447d374eb8c43c420a3df92267437d
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-11-07 10:58:56 +0100

    added sort_list

---
+++ .subjects/STUD_PART/exam_02/3/sort_list/main.c
@@ -0,0 +1,48 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void	swap_values(t_list *a, t_list *b);
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int));
+
+int ascending(int a, int b)
+{
+	return (a <= b);
+}
+
+int	main(void)
+{
+	t_list *c = malloc(sizeof(t_list));
+	c->next = 0;
+	c->data = 45;
+
+	t_list *b = malloc(sizeof(t_list));
+	b->next = c;
+	b->data = 73;
+
+	t_list *a = malloc(sizeof(t_list));
+	a->next = b;
+	a->data = 108;
+
+	t_list *cur = a;
+	while (cur)
+	{
+		printf("%d", cur->data);
+		if (cur->next != 0)
+			printf(", ");
+		cur = cur->next;
+	}
+	printf("\n");
+
+	cur = sort_list(a, ascending);
+
+	// cur = a;
+	while (cur)
+	{
+		printf("%d", cur->data);
+		if (cur->next != 0)
+			printf(", ");
+		cur = cur->next;
+	}
+	printf("\n");
+}
