cc2d5474525ad9106adaa199bc07311800081a42
Jo Cluzet <jo@jos-mac-studio.home>
2022-11-24 00:59:47 +0100

    fix putstr tester: add more test than one

--- .subjects/STUD_PART/exam_02/0/putstr/tester.sh
+++ .subjects/STUD_PART/exam_02/0/putstr/tester.sh
@@ -6,12 +6,24 @@
 #    By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/20 02:26:11 by jcluzet           #+#    #+#              #
-#    Updated: 2022/09/01 23:42:28 by jcluzet          ###   ########.fr        #
+#    Updated: 2022/11/24 00:58:44 by jcluzet          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FILE='ft_putstr.c'
 ASSIGN='ft_putstr'
+
+bash .system/auto_correc_main.sh $FILE $ASSIGN "Hello World!"
+if [ -e .system/grading/traceback ];then
+    mv .system/grading/traceback .
+	exit 1
+fi
+
+bash .system/auto_correc_main.sh $FILE $ASSIGN "Testinnnnng your puuuuuutstr!! :)"
+if [ -e .system/grading/traceback ];then
+    mv .system/grading/traceback .
+	exit 1
+fi
 
 bash .system/auto_correc_main.sh $FILE $ASSIGN
 if [ -e .system/grading/traceback ];then
