2b24d9e2f283bf19a0eb6684f88bd2f4d7a29d6e
Bruno B <83769496+septanteneuf@users.noreply.github.com>
2022-11-29 13:10:24 +0000

    putchar
    Why use put char instead of write ?

--- .subjects/STUD_PART/exam_02/0/first_word/first_word.c
+++ .subjects/STUD_PART/exam_02/0/first_word/first_word.c
@@ -12,11 +12,6 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 int	main(int ac, char **av)
 {
 	int	i;
@@ -24,14 +19,14 @@ int	main(int ac, char **av)
 	i = 0;
 	if (ac == 2)
 	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
+		while (av[1][i] == ' ' || av[1][i] == '\t') // si space ou tab passer au char suivant
 			i++;
-		while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
+		while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t') // si fin de chaine de char ET espace ET tab
 		{
-			ft_putchar(av[1][i]);
+			write(1, &av[1][i], 1); // alors print le texte 
 			i++;
 		}
 	}
-	ft_putchar('\n');
+	write(1, "\n", 1);
 	return (0);
 }
