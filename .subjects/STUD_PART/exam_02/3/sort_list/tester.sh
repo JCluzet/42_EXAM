1312321867df0f7c9f1780d9d3a50ed24f458d67
JCluzet <jo@cluzet.fr>
2022-12-27 02:12:04 +0100

    FIX [ExamRank02 lvl3 sort_list] now work without list.h as subject explanation

--- .subjects/STUD_PART/exam_02/3/sort_list/tester.sh
+++ .subjects/STUD_PART/exam_02/3/sort_list/tester.sh
@@ -3,20 +3,28 @@
 #                                                         :::      ::::::::    #
 #    tester.sh                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+         #
+#    By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/07 10:37:23 by pandalaf          #+#    #+#              #
-#    Updated: 2022/11/07 10:38:59 by pandalaf         ###   ########.fr        #
+#    Updated: 2022/12/27 02:09:44 by jcluzet          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FILE='sort_list.c'
 ASSIGN='sort_list'
 
+# Copy the file list.h from GradeMe to the student's folder thanks to @Kuninoto Issue
+cp .system/grading/list.h rendu/$ASSIGN/list.h
+
 bash .system/auto_correc_main.sh $FILE $ASSIGN
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
+    # Remove the file list.h from the student's folder
+    rm -f rendu/$ASSIGN/list.h
 	exit 1
 fi
+
+# Remove the file list.h from the student's folder
+rm -f rendu/$ASSIGN/list.h
 
 touch .system/grading/passed;
