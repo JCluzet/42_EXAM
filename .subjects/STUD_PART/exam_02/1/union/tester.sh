65c6ad6963b3880e88622c2ed9112d692341269f
galves46 <kindofariot@gmail.com>
2023-09-21 12:58:04 +0100

    fix for union correction error

--- .subjects/STUD_PART/exam_02/1/union/tester.sh
+++ .subjects/STUD_PART/exam_02/1/union/tester.sh
@@ -1,43 +1,43 @@
 FILE='union.c'
 ASSIGN='union'
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj"
+bash .system/auto_correc_program.sh $FILE $ASSIGN zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd
+bash .system/auto_correc_program.sh $FILE $ASSIGN ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN "rien" "cette phrase ne cache rien"
+bash .system/auto_correc_program.sh $FILE $ASSIGN "rien" "cette phrase ne cache rien"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN "rien"
+bash .system/auto_correc_program.sh $FILE $ASSIGN "rien"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN "a" "b"
+bash .system/auto_correc_program.sh $FILE $ASSIGN "a" "b"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN "  lorem,ipsum  " "oooo"
+bash .system/auto_correc_program.sh $FILE $ASSIGN "  lorem,ipsum  " "oooo"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN "this        ...       is sparta, then again, maybe    not" "lol"
+bash .system/auto_correc_program.sh $FILE $ASSIGN "this        ...       is sparta, then again, maybe    not" "lol"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
