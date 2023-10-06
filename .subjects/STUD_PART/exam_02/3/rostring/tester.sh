110ce0a184e9085047c39b9bac2a3202aa199fed
Jens Kulka <jkulka@2-E-8.42heilbronn.de>
2023-10-06 16:05:16 +0200

    Fix: Fixed missing union subject and added test case for rostring

--- .subjects/STUD_PART/exam_02/3/rostring/tester.sh
+++ .subjects/STUD_PART/exam_02/3/rostring/tester.sh
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    tester.sh                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+         #
+#    By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/07 00:45:37 by pandalaf          #+#    #+#              #
-#    Updated: 2022/11/07 01:41:49 by pandalaf         ###   ########.fr        #
+#    Updated: 2023/10/06 10:27:47 by jkulka           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -32,6 +32,12 @@ if [ -e .system/grading/traceback ];then
 fi
 
 bash .system/auto_correc_program.sh $FILE $ASSIGN "first" "2" "11000000"
+if [ -e .system/grading/traceback ];then
+	mv .system/grading/traceback .
+	exit 1
+fi
+
+bash .system/auto_correc_program.sh $FILE $ASSIGN "This is a test "
 if [ -e .system/grading/traceback ];then
 	mv .system/grading/traceback .
 	exit 1
