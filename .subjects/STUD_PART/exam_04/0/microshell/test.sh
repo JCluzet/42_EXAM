2de2fc440b3e7590ae1b92ec362c83c84fc7dd70
hcivici <153338245+hcivici@users.noreply.github.com>
2023-12-23 19:42:44 +0300

    fix and improvements for examrank04

--- .subjects/STUD_PART/exam_04/0/microshell/test.sh
+++ .subjects/STUD_PART/exam_04/0/microshell/test.sh
@@ -4,6 +4,8 @@ ulimit -n 30
 ./a.out /bin/ls microshell.c
 ./a.out /bin/ls salut
 ./a.out ";"
+./a.out ";abc"
+./a.out "|xyz"
 ./a.out ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";"
 ./a.out ";" ";" /bin/echo OK
 ./a.out ";" ";" /bin/echo OK ";"
@@ -27,4 +29,75 @@ ulimit -n 30
 ./a.out blah "|" /bin/echo OK ";"
 ./a.out ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" blah "|" /bin/echo OK ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";"
 ./a.out cd ";" /bin/ls
-./a.out /bin/ls "|" /usr/bin/grep microshell "|" /usr/bin/grep micro "|" /usr/bin/grep shell "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|"
+./a.out cd a b c ";" /bin/ls
+./a.out cd ./non_existing_directory ";" /bin/ls
+./a.out cd_but_fake ";" /bin/ls
+./a.out /bin/pwd ";" cd ";" /bin/pwd ";" cd /root extra_arg2 extra_arg3 ";"
+./a.out ";" cd ./microshell ";" /bin/pwd ";" /bin/ls "|" /bin/grep microshell.c ";" cd .. ";" ";" /bin/pwd ";" ";"
+./a.out /bin/ls "|" /usr/bin/grep microshell "|" /usr/bin/grep micro "|" /usr/bin/grep shell "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro
+
+KILLABLE_PNAME=/tmp/examrank04_killable.out
+pkill --signal SIGKILL -f $KILLABLE_PNAME
+cp a.out $KILLABLE_PNAME
+
+# waitpid right after every single fork
+($KILLABLE_PNAME /bin/sleep 2 "|" /bin/echo "#test_1 This line should be printed 1st" &)
+sleep 1
+./a.out /bin/echo "#test_1 This line should be printed 2nd"
+sleep 1
+pkill --signal SIGKILL -f $KILLABLE_PNAME
+
+# no waitpid at all
+(./a.out /bin/sleep 2 ";" /bin/echo "#test_2 This line should be printed 2nd" &)
+sleep 1
+./a.out /bin/echo "#test_2 This line should be printed 1st"
+sleep 1
+pkill --signal SIGKILL -f $KILLABLE_PNAME
+
+GPC_SOURCE=/tmp/get_pipe_capacity.c
+GPC_BINARY=/tmp/get_pipe_capacity.out
+PIPE_FILLER=/tmp/pipe_filler
+cat > $GPC_SOURCE << EOF
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int	fd[2];
+
+	pipe(fd);
+	printf("%d\n", fcntl(fd[0], F_GETPIPE_SZ));
+}
+EOF
+
+# creates a file that can fill up the pipe buffer capacity and has 1 extra byte that blocks final write
+test_full_pipe_buffer()
+{
+	gcc $GPC_SOURCE -o $GPC_BINARY #> /dev/null 2>&1
+	PIPE_CAPACITY=$($GPC_BINARY)
+	PIPE_CAPACITY_P1=$(($PIPE_CAPACITY + 1))
+	dd if=/dev/zero of=$PIPE_FILLER bs=1 count=$PIPE_CAPACITY_P1 > /dev/null 2>&1
+
+	cp a.out $KILLABLE_PNAME
+	($KILLABLE_PNAME /bin/cat $PIPE_FILLER "|" /bin/tee /dev/null &)
+	# assumptions:
+	# 1 - 'cat' can write all the bytes into the pipe and 'tee' can read all of them then both cmds exit. all under 1 second
+	# 2 - process uses waitpid() properly (last 2 checks above)
+	sleep 1 
+	PC_PGREP_OUTPUT=$(pgrep -f $KILLABLE_PNAME)
+	if [ -z $PC_PGREP_OUTPUT ]; then
+		echo "test_full_pipe_buffer: OK (Program doesn't rely on pipe buffer capacity)"
+	else
+		echo "test_full_pipe_buffer: KO (Program does    rely on pipe buffer capacity (Hint: Pipes need to get read from)"
+		pkill -f /bin/cat
+	fi
+}
+
+test_full_pipe_buffer
+pkill --signal SIGKILL -f $KILLABLE_PNAME
+rm -f $KILLABLE_PNAME
+rm -f $GPC_SOURCE
+rm -f $GPC_BINARY
+rm -f $PIPE_FILLER
