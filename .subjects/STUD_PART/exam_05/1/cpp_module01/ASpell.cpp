5293851a2f6d60814142eb54db4d05651c0accd9
Albedo <107936802+mrfadil10@users.noreply.github.com>
2024-09-05 15:58:14 +0100

    Update ASpell.cpp

--- .subjects/STUD_PART/exam_05/1/cpp_module01/ASpell.cpp
+++ .subjects/STUD_PART/exam_05/1/cpp_module01/ASpell.cpp
@@ -19,9 +19,9 @@ ASpell &ASpell::operator=(ASpell const &other)
 
 ASpell::~ASpell() {}
 
-std::string const &ASpell::getName() const { return (this->name);}
+std::string const ASpell::getName() const { return (this->name);}
 
-std::string const &ASpell::getEffects() const { return (this->effects);}
+std::string const ASpell::getEffects() const { return (this->effects);}
 
 void ASpell::launch(ATarget const &atarget_ref) const
 {
